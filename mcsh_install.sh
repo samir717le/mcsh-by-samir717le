ded9321b762123a98109ba976672b9c2296f002c
SAMIR 717 LE <158018273+samir717le@users.noreply.github.com>
2024-06-19 18:15:29 +0600

    mcsh_install.sh sudo

--- mcsh_install.sh
+++ mcsh_install.sh
@@ -1,8 +1,8 @@
-pkg install openjdk-17/stable -y
-pkg install wget dialog -y
+sudo apt install openjdk-17/stable -y
+sudo apt wget dialog -y
 clear
 wget -O mcsh https://gist.githubusercontent.com/samir717le/526f1c61dc2f3b583494ebcfd012ed8c/raw/a8acd989fc8fe971fd06fa5d5eb88da768e99258/gistfile1.txt
-mv mcsh $PREFIX/bin/mcsh
-chmod +x $PREFIX/bin/mcsh
+sudo mv mcsh /bin/mcsh
+sudo chmod +x /bin/mcsh
 mkdir ~/mcsh
 mkdir ~/mcsh/servers
