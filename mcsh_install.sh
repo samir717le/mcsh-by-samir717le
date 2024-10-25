0953a0900cd38b1351959c9e63f56e27a9e97ded
SAMIR 717 LE <158018273+samir717le@users.noreply.github.com>
2024-10-25 20:33:19 +0600

    Update mcsh_install.sh

--- mcsh_install.sh
+++ mcsh_install.sh
@@ -1,4 +1,4 @@
-sudo apt install openjdk-17/stable -y
+sudo apt install openjdk-17 -y
 sudo apt wget dialog -y
 clear
 wget -O mcsh https://gist.githubusercontent.com/samir717le/526f1c61dc2f3b583494ebcfd012ed8c/raw/a8acd989fc8fe971fd06fa5d5eb88da768e99258/gistfile1.txt
